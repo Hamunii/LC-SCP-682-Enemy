20484dc0bc98d297c5799d9a5afe81285ea0a5bf
Hamunii <samuel.suni04@gmail.com>
2024-05-22 22:10:15 +0300

    Initial commit

---
+++ Plugin/src/Plugin.cs
@@ -0,0 +1,98 @@
+﻿using System.Reflection;
+using UnityEngine;
+using BepInEx;
+using LethalLib.Modules;
+using BepInEx.Logging;
+using System.IO;
+using SCP682.Configuration;
+using System.Linq;
+
+namespace SCP682;
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency(LethalLib.Plugin.ModGUID)] 
+public class Plugin : BaseUnityPlugin {
+    internal static new ManualLogSource Logger = null!;
+    internal static PluginConfig BoundConfig { get; private set; } = null!;
+    public static AssetBundle? ModAssets;
+    internal static EnemyType SCP682ET = null!;
+
+    private void Awake() {
+        Logger = base.Logger;
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} started loading...");
+
+        // If you don't want your mod to use a configuration file, you can remove this line, Configuration.cs, and other references.
+        BoundConfig = new PluginConfig(base.Config);
+
+        // This should be ran before Network Prefabs are registered.
+        InitializeNetworkBehaviours();
+
+        // We load the asset bundle that should be next to our DLL file, with the specified name.
+        // You may want to rename your asset bundle from the AssetBundle Browser in order to avoid an issue with
+        // asset bundle identifiers being the same between multiple bundles, allowing the loading of only one bundle from one mod.
+        // In that case also remember to change the asset bundle copying code in the csproj.user file.
+        var bundleName = "scp682assets";
+#if DEBUG
+        var scriptsDir = Path.Combine(Paths.BepInExRootPath, "scripts");
+        ModAssets = AssetBundle.LoadFromFile(Path.Combine(scriptsDir, bundleName));
+#else
+        ModAssets = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Info.Location), bundleName));
+#endif
+        if (ModAssets is null) {
+            Logger.LogError($"Failed to load custom assets.");
+            return;
+        }
+
+        SCP682ET = ModAssets.LoadAsset<EnemyType>("SCP682ET");
+        var SCP682TN = ModAssets.LoadAsset<TerminalNode>("SCP682TN");
+
+        AddEnemyScript.SCP682AI(SCP682ET, ModAssets);
+
+        // Network Prefabs need to be registered. See https://docs-multiplayer.unity3d.com/netcode/current/basics/object-spawning/
+        // LethalLib registers prefabs on GameNetworkManager.Start.
+#if !DEBUG
+        NetworkPrefabs.RegisterNetworkPrefab(SCP682ET.enemyPrefab);
+#else
+        if(!Enemies.spawnableEnemies.Any(enemy => enemy.enemy.enemyName.Equals("SCP682")))
+#endif
+            Enemies.RegisterEnemy(SCP682ET, BoundConfig.SpawnWeight.Value, Levels.LevelTypes.All, SCP682TN);
+#if DEBUG
+        // We probably want the enemy to instantly spawn in front of us if possible
+        if(StartOfRound.Instance is not null)
+        {
+            Vector3 spawnPosition = GameNetworkManager.Instance.localPlayerController.transform.position - Vector3.Scale(new Vector3(-5, 0, -5), GameNetworkManager.Instance.localPlayerController.transform.forward);
+            RoundManager.Instance.SpawnEnemyGameObject(spawnPosition, 0f, -1, SCP682ET);
+        }
+#endif
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    // We should clean up our resources when reloading the plugin.
+    private void OnDestroy()
+    {
+        AddEnemyScript.ClearScript<SCP682AI>(SCP682ET.enemyPrefab);
+        ModAssets?.Unload(true);
+
+        SCP682AI.SCP682Objects.ForEach(Destroy);
+        SCP682AI.SCP682Objects.Clear();
+        
+        Logger.LogInfo("Cleaned all resources!");
+    }
+
+    private static void InitializeNetworkBehaviours() {
+        // See https://github.com/EvaisaDev/UnityNetcodePatcher?tab=readme-ov-file#preparing-mods-for-patching
+        var types = Assembly.GetExecutingAssembly().GetTypes();
+        foreach (var type in types)
+        {
+            var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (var method in methods)
+            {
+                var attributes = method.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    method.Invoke(null, null);
+                }
+            }
+        }
+    } 
+}
---
+++ SETUP-PROJECT.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+import os
+import shutil
+from subprocess import run
+from inspect import getsourcefile
+from contextlib import chdir
+
+# This is an automated script for copying required dll files into this project.
+# Supports both Windows and Linux.
+#
+# Also, this script got kinda out of hand towards the end. I won't waste more time on this though, it works anyways.
+# Feel free to contribute to this script
+
+class color:
+   reset = '\033[0m'
+   green = '\033[32m'
+   yellow = '\033[93m'
+   red = '\033[31m'
+   lightblue = '\033[94m'
+   lightcyan = '\033[96m'
+   purple = '\033[35m'
+   orange = '\033[33m'
+
+def exitProgram():
+   print(color.reset + 'Press Enter to close the program...')
+   input()
+   exit()
+
+def copyDLLs(sourceDir: str, destinationDir: str, dllList: list) -> bool:
+   if os.path.exists(fr'{sourceDir}'):
+      for dllFile in dllList:
+         shutil.copy2(f'{sourceDir}/{dllFile}', f'{destinationDir}/{dllFile}')
+         print(f'Got: {dllFile}')
+      return True
+   return False
+
+def main():
+   # Locate us
+   thisPath = os.path.dirname(os.path.realpath(getsourcefile(lambda:0)))
+   gameDataRelative = 'Lethal Company_Data/Managed'
+
+   # Locate the game's data folder
+   gameFilesPath = None
+   expectedGamePaths = ['C:/Program Files (x86)/Steam/steamapps/common/Lethal Company', f'{os.path.expanduser("~")}/.local/share/Steam/steamapps/common/Lethal Company']
+   for path in expectedGamePaths:
+      if os.path.exists(path):
+         gameFilesPath = path
+         break
+
+   if gameFilesPath is None:
+      print(color.yellow + "Could not locate Lethal Company game files!\nPlease paste the full path to 'Lethal Company':" + color.reset)
+      userInputGamePath = input()
+      if os.path.exists(userInputGamePath):
+         gameFilesPath = userInputGamePath
+      else:
+         print(color.red + "Could not find location.")
+         exitProgram()
+
+   if (gameFilesPath[-1] == "/" or gameFilesPath[-1] == '\\'):
+      userInputPath = f'{gameFilesPath}'
+
+   print(color.green + f'Game data path found: {gameFilesPath}' + color.reset)
+
+   # Make sure our Unity project still exists
+   unityProjectPath = f'{thisPath}/UnityProject'
+   unityPluginsRelative = 'Assets/Plugins'
+   if not os.path.exists(unityProjectPath):
+      print(color.yellow + f'Could not find Unity project at {unityProjectPath}! Paste the full path to your Unity project:' + color.reset)
+      userInputUnityPath = input()
+      if os.path.exists(userInputUnityPath):
+         unityProjectPath = userInputUnityPath
+      else:
+         print(color.red + "Could not find location.")
+         exitProgram()
+
+   if not os.path.exists(f'{unityProjectPath}/{unityPluginsRelative}'):
+      print(color.red + f"Your Unity Project does not have a {unityPluginsRelative} folder!\nMake sure your Unity Project is based on Lethal Company files.")
+      exitProgram()
+   print(color.lightblue + f'Unity Plugins path found: {unityProjectPath}/{unityPluginsRelative}' + color.reset)
+
+   # Copying dlls for Unity project
+   print('Copying game DLLs for Unity project:')
+   neededPluginDllFiles =[
+      "AmazingAssets.TerrainToMesh.dll",
+      "ClientNetworkTransform.dll",
+      "DissonanceVoip.dll",
+      "Facepunch Transport for Netcode for GameObjects.dll",
+      "Facepunch.Steamworks.Win64.dll",
+      "Newtonsoft.Json.dll",
+      "Assembly-CSharp-firstpass.dll"
+   ]
+   copyDLLs(f'{gameFilesPath}/{gameDataRelative}', f'{unityProjectPath}/{unityPluginsRelative}', neededPluginDllFiles)
+
+   print(color.green + f'Done copying game DLLs to {unityProjectPath}/{unityPluginsRelative}!' + color.reset)
+
+   #######################################################################################
+   # Run `dotnet tool restore`
+
+   print(color.lightblue + f'Part 1 of 3 complete!{color.purple}\nRunning `dotnet tool restore`')
+   with chdir(f'{thisPath}/Plugin/'):
+      try:
+         print(color.lightcyan + f'We are in: {os.getcwd()}{color.purple}')
+         run(["dotnet", "tool", "restore"]) 
+      except:
+         print(color.red + f'Error: failed to run command.')
+
+   #######################################################################################
+   # Generate .csproj.user file
+
+   print(color.lightblue + f'Part 2 of 3 complete!{color.purple}\n> Next you will have to provide a path to where we will copy your mod files each time your build it.\n'
+         
+         f'{color.orange}Examples:\n'
+         '     r2modman: /home/user/.config/r2modmanPlus-local/LethalCompany/profiles/testing/BepInEx/scripts\n'
+         '     Game installation: /home/user/.local/share/Steam/steamapps/common/Lethal Company/BepInEx/scripts')
+   print(color.lightcyan + f'Paste your path: ', end='')
+
+   userInputPath = input()
+   if not os.path.exists(userInputPath):
+      print(color.red + 'Path not found!')
+      exitProgram()
+
+   if not (userInputPath[-1] == "/" and not userInputPath[-1] == '\\'):
+      userInputPath = f'{userInputPath}/'
+
+   userFile = f"""<?xml version="1.0" encoding="utf-8"?>
+   <Project ToolsVersion="Current" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+      <!-- GENERATED BY SETUP-PROJECT.py -->
+      <PropertyGroup>
+         <!-- Automatically found or manually inputted game path -->
+         <GameDirectory>{gameFilesPath}/</GameDirectory>
+         <!-- The path you pasted when running the script -->
+         <PluginsDirectory>{userInputPath}</PluginsDirectory>
+      </PropertyGroup>
+
+      <!-- Game Directories - Do Not Modify -->
+      <PropertyGroup>
+         <ManagedDirectory>$(GameDirectory)Lethal Company_Data/Managed/</ManagedDirectory>
+      </PropertyGroup>
+      
+      <!-- Our mod files get copied over after NetcodePatcher has processed our DLL -->
+      <Target Name="CopyToTestProfile" DependsOnTargets="NetcodePatch" AfterTargets="PostBuildEvent">
+         <Copy SourceFiles="$(TargetPath)" DestinationFolder="$(PluginsDirectory)"/>
+         <!-- We will copy the asset bundle named "modassets" over -->
+         <Copy SourceFiles="../UnityProject/AssetBundles/StandaloneWindows/scp682assets" DestinationFolder="$(PluginsDirectory)"/>
+         <Exec Command="echo '[csproj.user] Mod files copied to $(PluginsDirectory)'" />
+      </Target>
+   </Project>"""
+
+   fp = open(f'{thisPath}/Plugin/SCP682.csproj.user', 'w')
+   fp.write(userFile)
+   fp.close()
+   print(color.green + f'csproj.user file created at {thisPath}/Plugin/SCP682.csproj.user!')
+
+   print(color.lightblue + f'Project Setup Complete!{color.lightcyan}\n> You should now be able to build the C# project, including the Asset Bundle!')
+   exitProgram()
+
+if __name__ == "__main__":
+   try:
+      main()
+   except Exception as exc:
+      print(color.red + "Something went wrong, and the setup script crashed!" + color.reset)
+      print(color.red + f"The error:\n{exc}" + color.reset)
+      print(color.yellow + "Make sure you run this script from the command line, like so: python SETUP-SCRIPT.py" + color.reset)
+      exitProgram()
